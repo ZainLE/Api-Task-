60b4a9432c66c9f4c5e6a0d950f1460baa1fae0e
ZainLatif7164 <latifzain40@gmail.com>
2023-12-08 22:21:45 +0100

    Text generator route

--- main.py
+++ main.py
@@ -1,7 +1,37 @@
-from flask import Flask
+from flask import Flask, render_template
+import openai
+from dotenv import load_dotenv
+import os
 
 app = Flask(__name__)
 
+user_input = input("Write some sentence to be completed: ")
+
+load_dotenv()
+openai.api_key = os.getenv('OPENAI_API_KEY')
+
 @app.route("/")
-def translator():
-   
+def text_complete():
+    try:
+        response = openai.Completion.create(
+            model="gpt-3.5-turbo-instruct",
+            prompt=user_input,
+            max_tokens=50
+    )
+
+        tagline = response['choices'][0]['text'].strip()
+
+        return render_template('index.html', tagline=tagline)
+    
+
+    except Exception as e:
+        return f"Error: {str(e)}"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
+
+
+
+
