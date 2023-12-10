009a85034364ed16cd8c0de667c1ed7bb81f2315
ZainLatif7164 <latifzain40@gmail.com>
2023-12-10 17:25:38 +0100

    Added Tts but tts is saved in ide only

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template
 import openai
 from dotenv import load_dotenv
 import os
+from pathlib import Path
+from gtts import gTTS
 
 app = Flask(__name__)
 
@@ -16,19 +18,25 @@ def text_complete():
         response = openai.Completion.create(
             model="gpt-3.5-turbo-instruct",
             prompt=user_input,
-            max_tokens=50
+            max_tokens=80
     )
 
         tagline = response['choices'][0]['text'].strip()
+        
+        speech_text = tagline
+        tts = gTTS(speech_text, lang='en')
+        speech_file_path = Path(__file__).parent / "speech.mp3"
+        tts.save(speech_file_path)
 
-        return render_template('index.html', tagline=tagline)
-    
-
+        return render_template('index.html', tagline=tagline, speech_file_path=str(speech_file_path))
+         
     except Exception as e:
         return f"Error: {str(e)}"
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
 
 
 
